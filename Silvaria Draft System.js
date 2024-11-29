05033071c62f2cad6223d3789c3ce90c73026e57
Boketto <104163073+Dollarxcade@users.noreply.github.com>
2024-11-29 08:14:35 -0500

    Update Silvaria Draft System.js

--- Silvaria Draft System.js
+++ Silvaria Draft System.js
@@ -7,12 +7,11 @@ function onOpen() {
 
 function snakeDraft() {
   const ss = SpreadsheetApp.getActiveSpreadsheet();
-  const draftSheet = ss.getSheetByName("Draft System Demo"); 
-  const playerDatabase = ss.getSheetByName("Players"); 
+  const draftSheet = ss.getSheetByName("Draft System Demo"); //replace with the name of the draft sheet that has 6 columns: 1 for the team draft number and the other 5 for their 5 draft picks do not have headers and start at A1
+  const playerDatabase = ss.getSheetByName("Players"); //replace with player database 
 
-  const players = playerDatabase.getRange(6, 5, 237 - 5, 5).getValues();  
-
-  players.sort((a, b) => b[0] - a[0]);
+  const players = playerDatabase.getRange(6, 4, 237, 5).getValues();  //first value is the row number, the second value is the column number (starts at 0 ex. A=1, B=1), the third value is the number of rows to fetch (downwards) and the fourth value is the number of columns to fetch (to the right)
+  players.sort((a, b) => b[0] - a[0]); //change value inside b and a with what columns it should sort with (ex. b[1]
 
   const draftData = draftSheet.getDataRange().getValues();
   const teamCount = draftData.length; 
